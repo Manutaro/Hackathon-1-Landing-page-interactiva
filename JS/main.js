cf030090b03b4b33a140a0a965dfeba51517e9d8
VanyaRz <vanyaberenice.ramirez@gmail.com>
2026-09-23 00:45:43 -0600

    Actualizaciones en Develop

--- JS/main.js
+++ JS/main.js
@@ -21,3 +21,24 @@ document.addEventListener("DOMContentLoaded", () => {//que cargue el archivo
         });
     });
 });
+
+// JS para carruseles
+// Se detienen al poner el cursor en la imagen y ya no avanzan
+const carousels = document.querySelectorAll('.carousel');
+carousels.forEach(c => {
+  c.addEventListener('mouseenter', () => {
+    bootstrap.Carousel.getInstance(c).pause();
+  });
+  c.addEventListener('mouseleave', () => {
+    bootstrap.Carousel.getInstance(c).cycle();
+  });
+});
+
+
+// Botón para subir 
+const btnTop = document.getElementById("btnTop");
+
+btnTop.onclick = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
