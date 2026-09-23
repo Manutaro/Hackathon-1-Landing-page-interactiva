1a723d5ec9a43c57607f184d444a90c826a01ccd
Lizeth Dorantes Gómez <lzethdg@gmail.com>
2026-09-23 10:14:24 -0600

    Refactor carousel initialization and button functionality

--- JS/main.js
+++ JS/main.js
@@ -1,11 +1,9 @@
 //Para reiniciar el carrusel de cada deporte
 document.addEventListener("DOMContentLoaded", () => {//que cargue el archivo
-    // 1. Se agregó el punto '.' para seleccionar la clase correctamente
     const enlaceDeportes = document.querySelectorAll(".btn-deporte"); 
 
     // Recorrer los botones cuando hay clic
     enlaceDeportes.forEach(enlace => {
-        // 2. Se cambió "clic" por "click"
         enlace.addEventListener("click", (e) => {
             const targetID = enlace.getAttribute("href");
             const targetSection = document.querySelector(targetID); 
