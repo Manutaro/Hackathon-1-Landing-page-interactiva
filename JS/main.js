12f6979e33743c3ec1bf3cab9953b80b4ee590db
VanyaRz <vanyaberenice.ramirez@gmail.com>
2026-09-22 20:35:17 -0600

    Se actualiza JS - Se detienen al poner el cursor en la imagen y ya no avanzan

---
+++ JS/main.js
@@ -0,0 +1,11 @@
+// JS para carruseles
+// Se detienen al poner el cursor en la imagen y ya no avanzan
+const carousels = document.querySelectorAll('.carousel');
+carousels.forEach(c => {
+  c.addEventListener('mouseenter', () => {
+    bootstrap.Carousel.getInstance(c).pause();
+  });
+  c.addEventListener('mouseleave', () => {
+    bootstrap.Carousel.getInstance(c).cycle();
+  });
+});
